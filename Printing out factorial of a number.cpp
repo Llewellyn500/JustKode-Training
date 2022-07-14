b9e5c9555319f94be3c53ccb1f0e8c14b6bef53e
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-07-14 19:42:17 +0000

    update

--- Printing out factorial of a number.cpp
+++ Printing out factorial of a number.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 	int n;
-	long double fact=1.0;
+	unsigned long long int fact=1;
 	cout << "Enter number =";
-	cin >> n;
+//	cin >> n;
 		do{
-			cout << "Factorial of a negative number does not exist" << endl;
-			cout << "Enter number =";
+//			cout << "Enter number =";
 			cin >> n;
+			cout << "Factorial of a negative number does not exist" << endl;
 		} while (n<0);
 			for (int i=1;i<=n;i++){
 				fact *=i;
