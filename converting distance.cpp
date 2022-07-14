f7a77c341e20ce22268fe655303a93e8d5edaf57
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-07-14 21:19:38 +0000

    Create converting distance.cpp

---
+++ converting distance.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double feet,miles,yards;
+	int distance;
+	cout<< "Enter distance:";
+	cin>>distance;
+	
+	miles=distance / 5280;
+	yards= (distance % 5280)/3;
+	feet= (distance % 5280) % 3;
+	
+	if(miles>0){
+		cout<<miles;
+		if(miles==1){cout<<" mile ";}else{cout<<" miles ";}
+	}
+	if (yards>0){
+		cout<<yards;
+		if(yards==1){cout<<" yard ";}else{cout<<" yards ";}
+	}
+	if(feet>0){
+		cout<<feet;
+		if(feet==1){cout<<" foot ";}else{cout<<" feet ";}
+		}
+		
+	
+			
+	return 0;
+}
