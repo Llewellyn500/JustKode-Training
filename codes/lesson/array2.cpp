e632c0cca275a4ecdf6936f5c7b879ecd5eae9d6
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-08-09 20:19:28 +0000

    started functions

---
+++ codes/lesson/array2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age[5] = {10, 15, 20, 25, 30};
+    cout << "1st age = ";
+    cout << age[0];
+    return 0;
+}
---
+++ codes/lesson/function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// a function to calculate the sum of 2 numbers
+int Anna(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << Anna(4, 2);
+    return 0;
+}
