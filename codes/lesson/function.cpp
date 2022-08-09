4b8b903ec5eb9f1799b8caa256dd32df3e271c17
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-08-09 20:53:46 +0000

    added function to display the name

--- codes/lesson/function.cpp
+++ codes/lesson/function.cpp
@@ -10,6 +10,13 @@ int Anna2(int a, int b)
 {
     return a * b;
 }
+// a function that takes a name as a parameter and print hello plus that name
+void Anna3(string name)
+{
+    cout << "Hello " << name << endl;
+}
+
+
 int main()
 {
     cout << "function to add 2 numbers: ";
@@ -18,5 +25,11 @@ int main()
 
     cout << "function to multiply 2 numbers: ";
     cout << Anna2(4, 2);
+    cout << endl;
+
+    // calling the function Anna3
+    Anna3("Anna");
     return 0;
 }
+
+
