3d98a5b7ee9afabefcd5c756a2ccdacae65c3266
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-08-09 20:40:46 +0000

    added function for multiplication of 2 numbers

--- codes/lesson/function.cpp
+++ codes/lesson/function.cpp
@@ -5,11 +5,18 @@ int Anna(int a, int b)
 {
     return a + b;
 }
-
+// a function to multiply 2 numbers
+int Anna2(int a, int b)
+{
+    return a * b;
+}
 int main()
 {
-    int x = 0;
-    Anna(1, 2);
-    // cout << Anna(4, 2);
+    cout << "function to add 2 numbers: ";
+    cout << Anna(4, 2);
+    cout << endl;
+
+    cout << "function to multiply 2 numbers: ";
+    cout << Anna2(4, 2);
     return 0;
 }
