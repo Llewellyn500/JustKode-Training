0f53a928cecc17c01be71c0c149396ad495bcfd3
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-09-08 21:03:05 +0000

    26

--- codes/class work/question 26.cpp
+++ codes/class work/question 26.cpp
@@ -1,23 +1,20 @@
 // Question 26
-// A prime number is any positive integer that is divisible by itself and the number 1. Write down a computer solution that can display as output all prime numbers less than or equal to 1000. 
+// A prime number is any positive integer that is divisible by itself and the number 1. Write down a computer solution that can display as output all prime numbers less than or equal to 1000.
 #include <iostream>
 using namespace std;
-int main(){
-    for (int i = 2; i <= 1000; i++)
+int main()
+{
+    int i, j, numOfFactors;
+    for (i = 2; i <= 1000; i++)
     {
-        bool is_prime = true;
-        for (int j = 2; j <= i / 2; j++)
+        numOfFactors = 0;
+        for (j = 1; j <= i; j++)
         {
             if (i % j == 0)
-            {
-                is_prime = false;
-                break;
-            }
+                numOfFactors++;
         }
-        if (is_prime)
-        {
-            cout << i << endl;
-        }
+        if (numOfFactors == 2)
+            cout << i << " is a prime number " << endl;
     }
     return 0;
 }
