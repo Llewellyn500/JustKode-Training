0e9a5b1c72865185c7074b2b460295d5652a9be6
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-09-08 21:59:15 +0000

    43

--- codes/class work/question 43.cpp
+++ codes/class work/question 43.cpp
@@ -1,4 +1,4 @@
 // Question 43
 // Modify your solution to question 42 such that all possible numbers of eggs that less than 500000 and their corresponding cost are displayed under appropriate headers.
 
-// I no dey barb the previous one nti i no fit solve this one
+#include<iostream>
