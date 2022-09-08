864977cc3bc158d67551201eef5a305dda297a92
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2022-09-08 21:10:37 +0000

    30

--- codes/class work/question 30.cpp
+++ codes/class work/question 30.cpp
@@ -2,17 +2,19 @@
 // Assume that no programming language has a function for returning the result of one integer raised to the power of another integer. It has become very necessary for you to write a computer solution that can be used to calculate a^b  where a and b are integers. Note that a may also be a real number but b should always be an integer.
 #include <iostream>
 using namespace std;
-int main(){
-    int a,b,c;
-    cout<<"Enter the number: ";
-    cin>>a;
-    cout<<"Enter the power: ";
-    cin>>b;
-    c=a;
-    while(b>1){
-        c=c*a;
+int main()
+{
+    int a, b, c;
+    cout << "Enter the number: ";
+    cin >> a;
+    cout << "Enter the power: ";
+    cin >> b;
+    c = a;
+    while (b > 1)
+    {
+        c = c * a;
         b--;
     }
-    cout<<"The result is: "<<c;
+    cout << "The result is: " << c;
     return 0;
 }
